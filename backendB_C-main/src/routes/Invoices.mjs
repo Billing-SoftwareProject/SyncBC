5cfa996caaefe84a6ae428377b1d847890053d74
Yash <yashmali702@gmail.com>
2023-12-21 15:33:43 +0530

    updated code
    Invoice vendor  Details fetching
    Revenue Fetching
    Revenue Formated

--- backendB_C-main/src/routes/Invoices.mjs
+++ backendB_C-main/src/routes/Invoices.mjs
@@ -1,15 +1,14 @@
-import invoicesController from '../controllers/Invoices.mjs';
 import express from 'express';
 const router = express.Router();
+import verifyToken from '../middleware.mjs'; // Import your authentication middleware.
 
-
-// import verifyToken from '../middleware.mjs'; // Import your authentication middleware.
-
+import invoicesController from '../controllers/Invoices.mjs';
 
 
 
 // Example route definition
-router.get('/invoices', invoicesController.getInvoices);
+router.get('/invoices', verifyToken,invoicesController.getInvoices);
+router.get('/invoicesitem/:Name',verifyToken, invoicesController.getInvoiceItems);
 router.post('/invoices', invoicesController.createInvoice);
 
 export default router;
